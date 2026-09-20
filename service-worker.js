03daa4adabc917896fc90ccb70ee287debe7c5f2
cnickerson-detailadmin <7w5zbbnhbm@privaterelay.appleid.com>
2026-09-20 16:45:26 -0400

    Refresh cache for PIN self-repair build

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "myservice-cache-v21";
-const BUILD_ID = "36-boot-self-heal";
+const CACHE_NAME = "myservice-cache-v22";
+const BUILD_ID = "37-pin-self-repair";
 
 const APP_SHELL = [
   "./styles.css?v=4-pro-desktop",
