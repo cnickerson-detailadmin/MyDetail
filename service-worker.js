f3156e4b721d1ff8a6eb77d330a72c909598a0ce
cnickerson-detailadmin <7w5zbbnhbm@privaterelay.appleid.com>
2026-09-20 02:05:13 -0400

    Add MyService offline app shell

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,40 @@
-const CACHE_NAME = "myservice-cache-v2";
+const CACHE_NAME = "myservice-cache-v3";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css?v=3",
+  "./app.js?v=16-pwa-install",
+  "./manifest.json"
+];
 
-self.addEventListener("install", () => {
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) =>
-        Promise.all(
-          cacheNames.map((cacheName) => caches.delete(cacheName))
-        )
-      )
+    caches.keys()
+      .then((names) => Promise.all(
+        names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      ))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./")))
+  );
 });
