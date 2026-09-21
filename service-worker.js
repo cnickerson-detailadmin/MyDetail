03c50fa91fa4636502db35187f856e50f0e5b2f6
cnickerson-detailadmin <7w5zbbnhbm@privaterelay.appleid.com>
2026-09-21 11:20:43 -0400

    Force refresh for updated Seth call interface

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "myservice-cache-v35";
-const BUILD_ID = "50-seth-cloudflare-voice";
+const CACHE_NAME = "myservice-cache-v36";
+const BUILD_ID = "51-seth-call-ui-diagnostics";
 
 const APP_SHELL = [
   "./styles.css?v=4-pro-desktop",
