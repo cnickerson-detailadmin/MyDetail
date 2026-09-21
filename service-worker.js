52869d3f68de8a99996206d5990826b11dabf7d3
cnickerson-detailadmin <7w5zbbnhbm@privaterelay.appleid.com>
2026-09-21 11:29:52 -0400

    Refresh login interaction build

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "myservice-cache-v37";
-const BUILD_ID = "52-business-ui-seth-audio";
+const CACHE_NAME = "myservice-cache-v38";
+const BUILD_ID = "53-login-interaction";
 
 const APP_SHELL = [
   "./styles.css?v=4-pro-desktop",
